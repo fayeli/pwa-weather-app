d5480053c5bdaa56868b2c3d80e7423b71163c67
Faye <si.fi.faye.li@gmail.com>
2020-05-12 15:30:47 +0800

    get user location with geolocation api

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React from 'react';
 import weather from './weather.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={weather} className="App-logo" alt="logo" />
-        <p>
-          Progressive Weather App
+class App extends React.Component {
+  state = {
+    latitude: null,
+    longitude: null,
+    locationErr: '',
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+
+  getLocation() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          locationErr: '',
+        })
+      },
+      (err) => {
+        this.setState({ locationErr: err.message});
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={weather} className="App-logo" alt="logo" />
+          <p>
+            Progressive Weather App
         </p>
-        <p className="App-blue-text">
-          <i>Coming Soon</i>
-        </p>
-      </header>
-    </div>
-  );
+          <p className="App-blue-text">
+            <i>Coming Soon</i>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
