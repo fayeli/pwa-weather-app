b2d4c44a4dc50cbb4f0dde85637440a710455759
Faye <si.fi.faye.li@gmail.com>
2020-05-15 00:39:08 +0800

    routing to city forecasts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import weatherSVG from './weather.svg';
 import OfflineBanner from './components/OfflineBanner';
 import SevenDayForecast from './components/SevenDayForecast';
+
+
+const cities = {
+  'hong-kong': {
+    name: 'Hong Kong',
+    coords: {
+      lat: 22.28552,
+      lon: 114.15769
+    }
+  },
+  'singapore': {
+    name: 'Singapore',
+    coords: {
+      lat: 1.28967,
+      lon: 103.85007
+    }
+  },
+  'san-francisco': {
+    name: 'San Francisco',
+    coords: {
+      lat: 37.774929,
+      lon: -122.419416
+    }
+  },
+  'tokyo': {
+    name: 'Tokyo',
+    coords: {
+      lat: 35.689487,
+      lon: 139.691706
+    }
+  }
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      location: null,
+      locationName: 'your current location',
+      locationCoords: null,
       locationError: null,
       locationErrorCode: null,
       isOffline: !window.navigator.onLine,
@@ -24,7 +58,7 @@ class App extends React.Component {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
-          location: {
+          locationCoords: {
             lat: position.coords.latitude,
             lon: position.coords.longitude
           },
@@ -33,7 +67,7 @@ class App extends React.Component {
         })
       },
       (err) => {
-        this.setState({ 
+        this.setState({
           locationError: err.message,
           locationErrorCode: err.code
         });
@@ -43,10 +77,29 @@ class App extends React.Component {
 
   monitorNetworkStatus() {
     window.addEventListener('offline', (event) => {
-      this.setState({isOffline: true});
+      this.setState({ isOffline: true });
     });
     window.addEventListener('online', (event) => {
-      this.setState({isOffline: false});
+      this.setState({ isOffline: false });
+    });
+  }
+
+  selectLocation = (city) => {
+    this.setState({
+      locationName: cities[city].name,
+      locationCoords: cities[city].coords,
+    });
+  }
+
+  renderCityList() {
+    return Object.keys(cities).map((key) => {
+      return (
+        <li key={key}>
+          <Link to={`/${key}`} onClick={() => this.selectLocation(key)}>
+            {cities[key].name}
+          </Link>
+        </li>
+      );
     });
   }
 
@@ -56,19 +109,29 @@ class App extends React.Component {
     return (
       <div className="App">
         {isOffline ? <OfflineBanner /> : null}
-        <header className="App-header">
-          <img src={weatherSVG} className="App-logo" alt="logo" />
-          <p>
-            Progressive Weather App
-          </p>
-          <p>7 Day Forecast for your current location</p>
-          <button onClick={this.getUserLocation} disabled={isOffline}>Check Now ➜</button>
-          {locationDenied ? <p>We're unable to get your location. Please check your permission settings and try again.</p> : null}
-          <SevenDayForecast location={this.state.location} isOffline={isOffline}/>
-          <p className="App-blue-text">
-            Forecast for your favourite cities <i>(Coming Soon)</i>
-          </p>
-        </header>
+        <BrowserRouter>
+          <header className="App-header">
+            <img src={weatherSVG} className="App-logo" alt="logo" />
+            <p>
+              Progressive Weather App
+            </p>
+
+            <p>7 Day Forecast for {this.state.locationName}</p>
+
+            <Route path="/" exact>
+              <button onClick={this.getUserLocation} disabled={isOffline}>Check Now ➜</button>
+              {locationDenied ? <p>We're unable to get your location. Please check your permission settings and try again.</p> : null}
+            </Route>
+
+            <SevenDayForecast location={this.state.locationCoords} isOffline={isOffline} />
+          </header>
+          <div className="App-blue-text">
+            <p>Forecast for your favourite cities <i>(Coming Soon)</i></p>
+            <ul>
+              {this.renderCityList()}
+            </ul>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
